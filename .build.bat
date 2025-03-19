ee0e9e0375de1383c7c40c60e1f46b752f2f1b2d
KAIYASITH <69749010+kaiyasith-la@users.noreply.github.com>
2025-03-19 19:02:34 +0700

    Update .build.bat

--- .build.bat
+++ .build.bat
@@ -7,3 +7,5 @@ third-party\common\win\premake\premake5.exe --file=premake5.lua --genproto --os=
 
 "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /nologo /v:n /p:Configuration=release,Platform=Win32 build\project\vs2022\win\gbe.sln
 "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /nologo /v:n /p:Configuration=release,Platform=x64 build\project\vs2022\win\gbe.sln
+
+pause
